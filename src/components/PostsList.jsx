80f9ff4952874f42bd808d44bda5e109f7099778
Abigail <abigailf03@hotmail.ca>
2024-05-09 09:55:06 -0500

    Added Routing
    intro to routing

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,62 +1,80 @@
-import {useState} from 'react';
-import classes from './PostsList.module.css';
-import Post from './Post';
-import Modal from './Modal.jsx'
-import NewPost from './NewPost';
+import { useState, useEffect } from "react";
+import classes from "./PostsList.module.css";
+import Post from "./Post";
+import Modal from "./Modal.jsx";
+import NewPost from "./NewPost";
 
-function PostsList({isPosting, onStopPosting}) {
-  const [posts, setPosts] = useState([]);//our posts array, setpost updating function
+function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]); //our posts array, setpost updating function
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      console.log.res
+      const response = await fetch('http://localhost:8080/posts');
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []);
 
   //triggered when post (form) is submitted
-  function addPostHandler(postData){
-    setPosts((existingPosts) => [postData, ... existingPosts]);
-
-    //setPosts([postData, ... posts]);
-    //this way above we use the spread operator only without the arrow func.
-    //similar but, better way to update state if it depends on the previous states
-    //snapshot. internally react doesn't run the state updatign funcs instantly,
-    //or not guarunteed, instead it schedules the states updates
-    //this way we used is a way of making sure that we don't use a old state. 
-    //makes sure that React ensures that you gettn he latest correct state for this state update,
-    //even if there ar emany pending state updates
+  function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
-//in the ul, we want to transform our array
-//of post objects into an array of JSX elements
-//one post element per post object
 
-//map - takes a function executed by the browser
-//for every item in this array receiving that item.
-//and then you return the value which this item should be mapped.
+  //in the ul, we want to transform our array
+  //of post objects into an array of JSX elements
+  //one post element per post object
 
-//map returns a new array where every item in that opld array
-//was transformed into a new item, into a new value
+  //map - takes a function executed by the browser
+  //for every item in this array receiving that item.
+  //and then you return the value which this item should be mapped.
 
-//here we transform every post in post array to a post JSX element
-    return (
+  //map returns a new array where every item in that opld array
+  //was transformed into a new item, into a new value
+
+  //here we transform every post in post array to a post JSX element
+  return (
     <>
-    {isPosting && (
-    <Modal onClose={onStopPosting} >
-       <NewPost 
-       onCancel={onStopPosting}
-       onAddPost={addPostHandler}
-      />
-    </Modal>
-    )}
-    {posts.length > 0 && (
-      <ul className={classes.posts}>
-        {posts.map((post) => (
-        <Post key={post.body} author={post.author} body={post.body}/>
-        ))}
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost 
+          onCancel={onStopPosting} 
+          onAddPost={addPostHandler} 
+          />
+        </Modal>
+      )}
+      {!isFetching && posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
         </ul>
-    )}
-    {posts.length === 0 && (
-      <div style={{textAlign: 'center', color: 'white'}}>
-        <h2>There are no posts yet.</h2>
-        <p>Start adding posts!</p>
-      </div>
-    )}
+      )}
+      {!isFetching && posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding posts!</p>
+        </div>
+      )}
+      {isFetching && (
+      <div style={{ textAlign: "center", color: "white" }}>
+        <p>Loading Posts...</p>
+        </div>
+      )}
     </>
-    );
+  );
 }
 
 export default PostsList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import App from './App.jsx'
+import NewPost from './components/NewPost';
 import './index.css'
+
+//call function to create route config object
+//store in const
+const router = createBrowserRouter([
+  {path: '/', element: <App /> },
+  {path: '/create-post', element: <NewPost />}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
